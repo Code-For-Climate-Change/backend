327466a650e9eda7b4546621a8ea1d8f26b875cf
Daisy Santos <daisy.santos@sumup.com>
2023-04-18 14:17:36 -0300

    Projeto Integrador task 03

---
+++ src/main/java/com/projeto/codecc/CodeccApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.codecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeccApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/projeto/codecc/model/Tema.java
@@ -0,0 +1,61 @@
+package com.projeto.codecc.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "A descrição é obrigatória")
+    @Size(max = 1000, message = "A descrição deve ter no minimo 10 e no máximo 1000 caracteres")
+    private String descricao;
+
+
+    @NotBlank(message = "O tema é obrigatória")
+    @Size(min = 5, max = 100, message = "A descrição deve ter no minimo 10 e no máximo 1000 caracteres")
+    private String tema;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getTema() {
+		return tema;
+	}
+
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+
+
+}
